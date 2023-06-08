efdbf43fad331a29915f353ce2a92608c274d2cc
fahrenh8T <raymondrayn98@gmail.com>
2023-06-08 23:09:53 +0300

    13-insert_number.c file updt

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,31 +11,31 @@
 */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_nd = NULL;
-    listint_t *cnt = *head;
-    listint_t *prv = NULL;
+	listint_t *new_nd = NULL;
+	listint_t *cnt = *head;
+	listint_t *prv = NULL;
 
-    new_nd = malloc(sizeof(listint_t));
-    if (new_nd == NULL)
-        return NULL;
-    new_nd->n = number;
-    new_nd->next = NULL;
+	new_nd = malloc(sizeof(listint_t));
+	if (new_nd == NULL)
+		return NULL;
+	new_nd->n = number;
+	new_nd->next = NULL;
 
-    if (*head == NULL || number < cnt->n)
-    {
-        new_nd->next = *head;
-        *head = new_nd;
-        return new_nd;
-    }
+	if (*head == NULL || number < cnt->n)
+	{
+		new_nd->next = *head;
+		*head = new_nd;
+		return new_nd;
+	}
 
-    while (cnt != NULL && number > cnt->n)
-    {
-        prv = cnt;
-        cnt = cnt->next;
-    }
+	while (cnt != NULL && number > cnt->n)
+	{
+		prv = cnt;
+		cnt = cnt->next;
+	}
 
-    new_nd->next = cnt;
-    prv->next = new_nd;
+	new_nd->next = cnt;
+	prv->next = new_nd;
 
-    return new_nd;
+	return new_nd;
 }
