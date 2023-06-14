ac314b39837ab7b2c94a1507e20d92eb55c3f196
fahrenh8T <raymondrayn98@gmail.com>
2023-06-14 22:07:59 +0300

    delete a line

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    '''function returns a list with all values multiplied by a number'''
     return list(map(lambda dict: dict * number, my_list))
