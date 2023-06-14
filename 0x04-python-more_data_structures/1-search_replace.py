9507dff3efa4b52a150e775fa05f9bee3818cf0d
fahrenh8T <raymondrayn98@gmail.com>
2023-06-14 19:37:15 +0300

    1-search_replace.py file updt

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    ''' function replaces all occurrence of an element
+    by another in a new list '''
     def find_search(element):
         if element != search:
             return (element)
