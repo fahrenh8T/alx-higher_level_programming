f0403d65b2f951de8d1b82dd9e00b89f12c14c2a
fahrenh8T <raymondrayn98@gmail.com>
2023-06-14 19:50:07 +0300

    0-square_matrix_simple.py file updt

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    '''function computes the square value of all integers of a matrix'''
     tempo = []
     for m in matrix:
         tempo.append(list(map(lambda m: m ** 2, m)))
