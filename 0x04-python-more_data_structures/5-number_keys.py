20a8664ca745e2efad89ed3eeecf33a93bd00b01
fahrenh8T <raymondrayn98@gmail.com>
2023-06-14 20:12:25 +0300

    add line

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,4 @@
 def number_keys(a_dictionary):
-    '''function returns the length/number of keys in a dictionary'''
-    return (len(a_dictionary))
+    '''function returns the length of dictionary'''
+    length = len(a_dictionary)
+    return (length)
