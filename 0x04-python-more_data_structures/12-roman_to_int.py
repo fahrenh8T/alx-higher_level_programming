6cd39d6433d04e3ba1de4e931b5f35c230343b98
fahrenh8T <raymondrayn98@gmail.com>
2023-06-14 23:01:03 +0300

    add description

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    '''convers Roman numeral to integer'''
     if not roman_string or type(roman_string) != str:
         return 0
     dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
