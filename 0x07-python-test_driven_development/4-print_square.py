4fd9749ca856b56e4281e585ca0898e4b07eedc7
fahrenh8T <raymondrayn98@gmail.com>
2023-07-05 03:50:14 +0300

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     elif isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    for n in range(0, size):
-        for m in range(size):
+    for row in range(0, size):
+        for col in range(size):
             print("#", end="")
         print("")
