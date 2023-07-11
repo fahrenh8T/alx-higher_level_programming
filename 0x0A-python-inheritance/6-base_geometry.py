b03b9a88df098f7e3e0929bd59d5e82f7cd8546e
fahrenh8T <raymondrayn98@gmail.com>
2023-07-11 03:41:49 +0300

    6-base_geometry.py files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''module: 6-base_geometry
+'''
+
+
+class BaseGeometry:
+    '''class: BaseGeometry
+    base class representing geometric entities
+    '''
+
+    def area(self):
+        '''function:
+    calculate the area of the geometric entity
+        '''
+        raise Exception("area() is not implemented")
