34f7a4be0fc9212e0fc138a224a90554b65da1f3
fahrenh8T <raymondrayn98@gmail.com>
2023-07-11 04:43:02 +0300

    10-square.py files

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module: square
+    contains the definition of a Square class derived from the Rectangle class.
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class: Square
+        represents a square object
+    '''
+    def __init__(self, size):
+        '''function: __init__
+        initialize a Square instance.
+        Args:
+        size (int): The size of the square.
+        '''
+        self.__size = size
+        super().__init__(self.__size, self.__size)
