89e68d8e90c27b854c090a221d3feb77af3f0a7f
fahrenh8T <raymondrayn98@gmail.com>
2023-07-11 17:06:10 +0300

    3-to_json_string.py files

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''module: 3-to_json_string
+    function that returns the JSON representation of an object (string)
+'''
+
+import json
+
+
+def to_json_string(my_obj):
+    '''function: to_json_string
+        module to_json_strin
+    '''
+    return json.dumps(my_obj)
