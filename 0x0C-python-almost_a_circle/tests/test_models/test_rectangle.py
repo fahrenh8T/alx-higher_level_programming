e5794457480f7bc98c1fa5b4f3e34909a94c5b48
fahrenh8T <raymondrayn98@gmail.com>
2023-07-20 05:20:18 +0300

    tests/test_models/ modified

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''module: test_rectangle
-   test class Rectangle
-'''
-import unittest
-import json
-import sys
-import os
-from models.rectangle import Rectangle
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-'''module: test_square
-    test class Square'''
-import unittest
-from models.square import Square
-import sys
-from io import StringIO
