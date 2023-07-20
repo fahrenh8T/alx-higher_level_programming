7ce9ba71c784e14ae53296953d57865831db5ec7
fahrenh8T <raymondrayn98@gmail.com>
2023-07-20 05:26:30 +0300

    test dir repatched

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''module: test_rectangle
+   test class Rectangle
+'''
+import unittest
+import json
+import sys
+import os
+from models.rectangle import Rectangle
---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''module: test_square
+    unittest class Square'''
+import unittest
+from models.square import Square
+import sys
+from io import StringIO
