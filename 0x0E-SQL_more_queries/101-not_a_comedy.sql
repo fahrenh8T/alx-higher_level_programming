bb63c72bb74487cfc2b323e8a834ae7ccd1eca95
fahrenh8T <raymondrayn98@gmail.com>
2023-08-17 05:38:24 +0300

    101-not_a_comedy.sql updt2

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,9 +5,9 @@ FROM tv_shows
 WHERE tv_shows.id NOT IN
 (SELECT tv_shows.id
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_shows_genres.show_id
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy")
 ORDER BY tv_shows.title;
