6e327b892ec6a68ad42bae6bf824b55ffeb07ec9
fahrenh8T <raymondrayn98@gmail.com>
2023-08-17 05:30:21 +0300

    100-not_my_genres.sql updt

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,7 +7,7 @@ WHERE tv_genres.id NOT IN
 (SELECT tv_genres.id
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres_id
+ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter")
