aea9ea94032e016c63887114d976a860dc2c29d5
fahrenh8T <raymondrayn98@gmail.com>
2023-09-13 04:58:48 +0300

    10-converter.js file patch2

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 // converts nunber from base 10 to another base
 exports.converter = function (base) {
-  function myConverter (num) {
-    return num.toString(base);
-  }
+  return num => num.toString(base);
 };
