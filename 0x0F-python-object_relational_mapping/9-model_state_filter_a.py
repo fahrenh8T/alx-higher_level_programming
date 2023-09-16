280c5caeb83c4b1c80c834377ae5ae5580f4375b
fahrenh8T <raymondrayn98@gmail.com>
2023-09-16 20:14:43 +0300

    9-model_state_filter_a.py patch1

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ prints the first State object from the database hbtn_0e_6_usa
 """
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -9,10 +9,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     sql_engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                     .format(argv[1], argv[2], argv[3]))
+
     Base.metadata.create_all(sql_engine)
     Session = sessionmaker(bind=sql_engine)
-    
+
     session = Session()
 
     for state in session.query(State).filter(State.name.like('%a%')):
