de9f14e0c35e07c2949a9afffc16ececf516b161
fahrenh8T <raymondrayn98@gmail.com>
2023-09-16 18:41:31 +0300

    4-cities_by_state.py patch1

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     """
 
     db_connect = sql.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+                             user=argv[1], passwd=argv[2], db=argv[3])
 
     with db_connect.cursor() as db_cursor:
         db_cursor.execute("SELECT cities.id, cities.name, states.name \
