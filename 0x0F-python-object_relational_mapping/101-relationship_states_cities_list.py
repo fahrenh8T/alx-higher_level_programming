2ced5d1ad0046d1274cc9ffd67afb6202d21dab5
fahrenh8T <raymondrayn98@gmail.com>
2023-09-17 16:51:06 +0300

    101-relationship_states_cities_list.py patch1

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,9 +10,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    dbengine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    dbengine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+                argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=dbengine)
     session = Session()
