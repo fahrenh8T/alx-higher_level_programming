db182443b8ba07b9d030bb0998b04220060cca3c
fahrenh8T <raymondrayn98@gmail.com>
2023-09-29 11:14:48 +0300

    1-hbtn_header.py updt

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" script takes in a URL, sends a request
+""" script takes in a URL, sends a request 
     displays the value=X-Request-Id variable
-    found in the header of the response
+- found in the header of the response
 """
 import sys
 import urllib.request
