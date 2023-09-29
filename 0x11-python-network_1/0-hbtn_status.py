498b0f8659bfa58258435d67515c6fc9e752fa9d
fahrenh8T <raymondrayn98@gmail.com>
2023-09-29 10:50:23 +0300

    my status; 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        shape = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(shape)))
+        print("\t- content: {}".format(shape))
+        print("\t- utf8 content: {}".format(shape.decode('utf-8')))
